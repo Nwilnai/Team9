903729bae78319b5d15db4694893ef67ec8ac228
jaredpanson <jaredpanson@gmail.com>
2021-03-07 10:53:27 -0500

    Completed chapters 3 and 5 of RORT

---
+++ app/controllers/user_pages_controller.rb
@@ -0,0 +1,4 @@
+class UserPagesController < ApplicationController
+  def login
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  get 'users/new'
+  root 'static_pages#home'
+  get  '/home',     to: 'static_pages#home'
+  get  '/help',     to: 'static_pages#about'
+  get  '/signup',   to: 'users#new'
 end
