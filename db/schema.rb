7529b44c21e8ced8c45c907fe0e8accf8d84d7ed
Nathaniel Wilnai <nmwilnai@gmail.com>
2021-03-08 12:42:20 -0500

    fixed database and added migration for index to user email

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_222354) do
+ActiveRecord::Schema.define(version: 2021_03_08_171438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "courses", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "code"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "instructors", force: :cascade do |t|
-    t.text "name"
-    t.string "email"
-    t.string "instructor_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "subjects", force: :cascade do |t|
-    t.text "name"
-    t.string "subject_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -44,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_07_222354) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
