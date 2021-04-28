5ddc5c5b165c28481d49abbb4c4a0996a7d566fd
Nathaniel Wilnai <nmwilnai@gmail.com>
2021-04-27 23:11:48 -0400

    undid changes, commented stylesheet out in application.html.erb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
 import "bootstrap"
-import '../stylesheets/application';
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
